0da5e040011c1a6c1b9415ead0d359cb7c8b8d8a
Ivan-Samoylenko <ivansamoylenko@ukr.net>
2022-08-04 13:54:43 +0300

    add solved task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+const largeImage = basicLightbox.create(`<img>`, {
+  onShow: () => window.addEventListener('keydown', onEscape),
+  onClose: () => window.removeEventListener('keydown', onEscape),
+});
+
+gallery.addEventListener('click', onClickGallery);
+
+function getGalleryItemMarkup() {
+  return galleryItems
+    .map(item => {
+      return `
+      <div class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+          <img
+              class="gallery__image"
+              src="${item.preview}"
+              data-source="${item.original}"
+              alt="${item.description}"
+            />
+        </a>
+      </div>
+    `;
+    })
+    .join('');
+}
+
+gallery.innerHTML = getGalleryItemMarkup();
+
+function onClickGallery(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  largeImage.element().querySelector('img').setAttribute('src', `${event.target.dataset.source}`);
+  largeImage.show();
+}
+
+function onEscape(event) {
+  if (event.code !== 'Escape') {
+    return;
+  }
+  largeImage.close();
+  largeImage.element().querySelector('img').removeAttribute('src');
+}
 
 console.log(galleryItems);
